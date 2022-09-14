97427cd9adba902e7902bac7fcf342faf8725a28
Emmanuel Ogbonna <diadimanuel@gmail.com>
2022-09-13 19:15:03 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints from a - z
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	for (int i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
