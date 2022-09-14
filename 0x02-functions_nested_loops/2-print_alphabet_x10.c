047c0f68678d7f482f7f65b5ae915d7e8abaffa1
Emmanuel Ogbonna <diadimanuel@gmail.com>
2022-09-13 20:36:04 -0700

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+	c = print_alphabet_x10();
+
+	_putchar(print_alphabet_x10());
+	return (0);
+}
