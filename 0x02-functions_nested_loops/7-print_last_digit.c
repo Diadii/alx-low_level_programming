8b10788e1501fff5916943024ac1ed42eaded1bd
Emmanuel Ogbonna <diadimanuel@gmail.com>
2022-09-14 20:29:56 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,10 @@ int print_last_digit(int n)
 
 	last_digit = n % 10;
 
-	_putchar(last_digit);
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+	_putchar('0' + last_digit);
 	return (last_digit);
 }
